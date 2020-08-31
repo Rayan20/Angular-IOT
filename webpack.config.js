7de150b53d6749067004c813caa9e4cf6875cff4
Yang, Richard <Richard.Yang@pearson.com>
2020-08-30 23:49:29 -0500

    debugging databse query

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,13 @@ module.exports = {
   entry: './server.js',
   mode: 'production',
   target: 'node',
-    alias: {
-      'pg-native': './donotdelete.js',
-    'dns': './donotdelete.js'
-    },
+    // alias: {
+    //   'pg-native': './donotdelete.js',
+    // 'dns': './donotdelete.js'
+    // },
+    plugins: [
+        new webpack.IgnorePlugin(/^pg-native$/)
+    ],
   output: {
     path: path.resolve(__dirname, '.'),
     filename: 'server.bundle.js'
